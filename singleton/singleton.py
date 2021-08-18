52986f63a14f36b34ab8bef0d57e824a90b00d1e
Jailton Silva <jss1@ifal.edu.br>
2021-08-17 23:51:32 -0300

    feat: adding Singleton class

--- singleton/singleton.py
+++ singleton/singleton.py
@@ -1,1 +1,27 @@
+class Singleton:
+    def __new__(cls):
+        """
+        The python's __new__ method runs before __init__
+        """
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+            print(f'Creating {cls.instance} object')
+        return cls.instance
 
+
+def main() -> None:
+    """
+    Shows that only a single instance is created
+    :return: None
+    """
+    s1 = Singleton()
+    print(f'Id of instance 1: {id(s1)}')
+
+    dict_singleton = {}
+    for index in range(2, 11):
+        dict_singleton[index] = Singleton()
+        print(f'Id of instance {index}: {id(dict_singleton[index])}')
+
+
+if __name__ == '__main__':
+    main()
